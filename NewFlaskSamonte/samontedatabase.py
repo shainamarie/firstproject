3451804be890d3836a0657b5d4bd48235d2e6d69
shanana <sweetmarie980@gmail.com>
2017-11-21 17:55:32 +0800

    Update samontedatabase.py

--- NewFlaskSamonte/samontedatabase.py
+++ NewFlaskSamonte/samontedatabase.py
@@ -3,9 +3,9 @@ import sys
 
 
 students = (
-    ('0', 'Shaina Marie', 'Samonte', '20152567', 'cutiesshaina98@gmail.com', 'nanana' ),
-    ('1', 'James', 'Lavilla', '20150444', 'jacob', 'jems'),
-    ('2', 'Karyl', 'Sabulbero', '20150001', 'carla', 'luhhh')
+    ('2015-2567', 'Shaina Marie', 'Anim', 'Samonte', 'F', 'BSCS','3' ),
+    ('2015-0444', 'James', 'Villa', 'Lavilla', 'M', 'BSIT', '3'),
+    ('2015-2356', 'Karyl', 'Lambayong', 'Sabulbero', 'F', 'BSCS', '3')
 )
 
 con = lite.connect('samontedatabase.db')
@@ -15,5 +15,21 @@ with con:
     cur = con.cursor()
 
     cur.execute("DROP TABLE IF EXISTS stud")
-    cur.execute("CREATE TABLE stud(id INTEGER PRIMARY KEY, fname TEXT, lname TEXT, idno INT, email TEXT, password TEXT )")
-    cur.executemany("INSERT INTO stud VALUES(?, ?, ?, ?, ?, ?)", students)
+    cur.execute("CREATE TABLE stud(id TEXT PRIMARY KEY, fname TEXT, mname TEXT, lname TEXT, gender INT, course TEXT, year TEXT )")
+    cur.executemany("INSERT INTO stud VALUES(?, ?, ?, ?, ?, ?, ?)", students)
+
+    cur.execute(
+        'CREATE TABLE IF NOT EXISTS studcourses(COURSE_ID TEXT PRIMARY KEY, DESCRIPTION TEXT, COLLEGE TEXT)')
+    cur.execute(
+        "INSERT OR IGNORE INTO studcourses(COURSE_ID, DESCRIPTION, COLLEGE) VALUES ('BSCS', 'Bachelor of Science in Computer Science', 'SCS')")
+    cur.execute(
+        "INSERT OR IGNORE INTO studcourses(COURSE_ID, DESCRIPTION, COLLEGE) VALUES ('BSIT', 'Bachelor of Science in Information Technology', 'SCS')")
+    cur.execute(
+        "INSERT OR IGNORE INTO studcourses(COURSE_ID, DESCRIPTION, COLLEGE) VALUES ('BSECT', 'Bachelor of Science in Electronics and Computer Technology', 'SCS')")
+    cur.execute(
+        "INSERT OR IGNORE INTO studcourses(COURSE_ID, DESCRIPTION, COLLEGE) VALUES ('ECET', 'Electronics Engineering Technology', 'SCS')")
+
+    cur.execute(
+        "CREATE VIEW IF NOT EXISTS courseRecord AS SELECT COURSE_ID, lname, fname, DESCRIPTION, COLLEGE, year FROM stud CROSS JOIN studcourses WHERE studcourses.COURSE_ID = stud.course")
+    cur.execute(
+        "CREATE VIEW IF NOT EXISTS infoComplete AS SELECT  id, COURSE_ID, DESCRIPTION, COLLEGE, lname, fname, mname, gender, year FROM stud CROSS JOIN studcourses WHERE studcourses.COURSE_ID = stud.course")
